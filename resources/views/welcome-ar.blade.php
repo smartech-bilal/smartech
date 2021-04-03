2c6c6ac9dedf431352dcb5583fb14f64d2945ec3
katbi89 <katbi89@gmail.com>
2021-04-03 19:26:34 +0300

    add files

---
+++ resources/views/welcome-ar.blade.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>سمارتك</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/fontAwesome.css">
+        <link rel="stylesheet" href="css/templatemo-style-ar.css">
+
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+
+        <div class="overlay"></div>
+        <section class="top-part">
+          <video controls autoplay loop>
+            <source src="videos/video.mp4" type="video/mp4">
+            <source src="videos/video.ogg" type="video/ogg">
+          Your browser does not support the video tag.
+          </video>
+        </section>
+        
+        <section class="cd-hero">
+
+          <div class="cd-slider-nav">
+            <nav>
+              <span class="cd-marker item-1"></span>
+              <ul>
+              <li><a href="#0"><div class="image-icon"><img src="img/contact-icon.png"></div><h6>اتصل بنا</h6></a></li>
+              <li><a href="#0"><div class="image-icon"><img src="img/projects-icon.png"></div><h6>عن المركز</h6></a></li>
+              <li><a href="#0"><div class="image-icon"><img src="img/featured-icon.png"></div><h6>خدماتنا</h6></a></li>
+              <li><a href="#0"><div class="image-icon"><img src="img/about-icon.png"></div><h6>الكورسات</h6></a></li>
+                <li class="selected"><a href="#0" style="font-family: 'Cairo', sans-serif;"><div class="image-icon"><img src="img/home-icon.png"></div><h6>الرئيسية</h6></a></li>
+                <bi><a href="/" style="text-decoration: none;"><div class="image-icon" style="margin: 0 auto;"><img src="img/lang-icon.png"></div><h6 style="font-size: 12px;color: white;">English</h6></a></bi>
+              </ul>
+            </nav> 
+          </div> <!-- .cd-slider-nav -->
+
+          <ul class="cd-hero-slider">
+
+          <li>
+              <div class="heading">
+                <h1>اتصل بنا</h1>
+                <span>يسرنا اتصالك</span> 
+              </div>
+              <div class="cd-half-width fivth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fivth-content">
+                        <div class="row">
+
+                          <div class="col-md-8 text-right">
+                            <div class="row">
+                              <form id="contact" action="" method="post">
+                                <div class="col-md-6">
+                                <fieldset>
+                                    <input name="email" type="email" class="form-control text-right" id="email" placeholder="البريد الالكتروني" required>
+                                  </fieldset>
+
+                                </div>
+                                <div class="col-md-6">
+                                <fieldset>
+                                    <input name="name" type="text" class="form-control text-right" id="name" placeholder="اسمك" required>
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <textarea name="message" rows="6" class="form-control text-right" id="message" placeholder="رسالتك" required></textarea>
+                                  </fieldset>
+                                </div>
+                                <div class="col-md-12">
+                                  <fieldset>
+                                    <button type="submit" id="form-submit" class="btn">أرسل الرسالة</button>
+                                  </fieldset>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                          <div class="col-md-4 text-right">
+                            <div class="left-info">
+                              <p>مزة فيلات شرقية
+                              <br>بناء الصفدي<br>
+                              <em><br></em>
+                              </p>
+                              <ul class="social-icons text-right">
+                                <i><a href="#"><i class="fa fa-facebook"></i></a></i>
+                                <i><a href="#"><i class="fa fa-twitter"></i></a></i>
+                                <!-- <i><a href="#"><i class="fa fa-linkedin"></i></a></i> -->
+                                <!-- <i><a href="#"><i class="fa fa-rss"></i></a></i> -->
+                                <!-- <i><a href="#"><i class="fa fa-behance"></i></a></i> -->
+                              </ul>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>من نحن؟</h1>
+                <span>معلومات أكثر عن مركزنا</span> 
+              </div>
+              <div class="cd-half-width second-slide">   
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content second-content">
+                        <div class="row">
+                          <div class="col-md-7 left-image">
+                            <img src="img/left-about-image.jpg">
+                          </div>
+                          <div class="col-md-5">
+                            <div class="right-about-text">
+                              <h4>من نحن ؟</h4>
+                              <p>معلومات عن المركز</p>
+                              <div class="primary-button">
+                                <a href="#">اكتشف المزيد</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="row">
+                          <div class="col-md-7">
+                            <div class="left-about-text">
+                              <h4>رسالتنا</h4>
+                              <p>رسالة المركز</p>
+                              <div class="primary-button">
+                                <a href="#">اكتشف المزيد</a>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="col-md-5 right-image">
+                            <img src="img/right-about-image.jpg">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>خدماتنا</h1>
+                <span>نقدم كافة خدمات الصيانة والشبكات</span> 
+              </div>
+              <div class="cd-half-width third-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content third-content">
+                        <div class="row">
+                          <div class="col-md-7 left-image">
+                            <img src="img/left-feature-image.png">
+                          </div>
+                          <div class="col-md-5">
+                            <div class="right-feature-text">
+                              <h4>نوع الخدمة: <em>شبكات</em></h4>
+                              <p>وصف عن الخدمة المذكورة</p>
+                              <div class="feature-list">
+                                <ul>
+                                  <p>- ميزة</p>
+                                  <p>- ميزة</p>
+                                  <p>- ميزة</p>
+                                  <p>- ميزة</p>
+                                  <p>- ميزة</p>
+                                </ul>
+                              </div>
+                              <div class="primary-button">
+                                <a href="#">اكتشف المزيد</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <div class="heading">
+                <h1>الكورسات</h1>
+                <span>هنا ستجد كافة الكورسات الجديدة في المركز</span> 
+              </div>
+              <div class="cd-half-width fourth-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content fourth-content">
+                        <div class="row">
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-01.jpg" data-lightbox="image-1"><img src="img/project-item-01.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-02.jpg" data-lightbox="image-1"><img src="img/project-item-02.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-03.jpg" data-lightbox="image-1"><img src="img/project-item-03.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-04.jpg" data-lightbox="image-1"><img src="img/project-item-04.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-05.jpg" data-lightbox="image-1"><img src="img/project-item-05.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-06.jpg" data-lightbox="image-1"><img src="img/project-item-06.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-07.jpg" data-lightbox="image-1"><img src="img/project-item-07.jpg"></a>
+                          </div>
+                          <div class="col-md-3 project-item">
+                            <a href="img/item-08.jpg" data-lightbox="image-1"><img src="img/project-item-08.jpg"></a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+
+
+
+            <li class="selected">
+              <div class="heading">
+                <h1>مركز سمارتك</h1>
+                <span>أهلا بكم في مركز سمارتك</span>
+              </div>
+              <div class="cd-full-width first-slide">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="content first-content">
+                        <h4>وصف بسيط عن المركز</h4>
+                        <p> <a href="#" target="_blank"></a>  <a href="#" target="_blank"></a></p>
+                        <div class="primary-button">
+                          <a href="#">اكتشف المزيد</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>
+              </div>
+            </li>
+          </ul> <!-- .cd-hero-slider -->
+        </section> <!-- .cd-hero -->
+
+
+        <footer>
+          <p>كافة الحقوق محفوظة &copy; 2021 سمارتك 
+                                
+        	| تصميم وبرمجة <em>بلال كاتبي</em></p>
+        </footer>
+    
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main-ar.js"></script>
+
+    </body>
+</html>
